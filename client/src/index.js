7382096a511038e683a1a97070cef64ee863e44b
NguyenTuTrung <trungcnttvnu@gmail.com>
2024-02-25 23:42:34 +0700

    last version

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import StudentManagement from "./StudentManagement";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(<StudentManagement />, document.getElementById("root"));
+
+reportWebVitals();
